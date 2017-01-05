94e83d82c9ff8692a430904feff288f19aa1567f
AbiyMelaku <agirma08@gmail.com>
2017-01-05 14:06:39 -0800

    finished up initialize and render of SongQueueView
    The song queue view is the view for the entire collection of songs in the queue.
    It needs to render each view for each song model.

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,33 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
-  },
+    //tagname
+    tagname: 'table',
 
+    this.collection.on('add remove', this.render, this);
+
+
+  },
+//creates SongQueueEntryViews for each queued song & renders them
   render: function() {
+    //some backbone source code and in all the parent views I see the following line of code:
+    this.$el.children().detach(); //optional?????
+     //The .detach() method is the same as .remove(), except that .detach() 
+    //keeps all jQuery data associated with the removed elements. 
+    //This method is useful when removed elements are to be 
+    //reinserted into the DOM at a later time.
+
+    this.$el.html('<th>Song Queue</th>').append(this.collection.map(function(song) {
+      return new SongQueueEntryView({model: song}).render();//expected render to have been called at least once, but it was never called
+    }));
+
     return this.$el;
   }
-
+// this.$el.find('ul').append(this.collection.map(function(comment) {
+//       // YOUR CODE HERE
+//       console.log(comment);
+//       return '<li>' + comment.get('message') + '   --------------------------' + comment.get('votes') + '</li>'
+//     }));
+//     return this.$el;
+//   }
 });
