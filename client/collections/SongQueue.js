65fd0c0d0e0c8f9d319d0f5ee9126ae8fed493c9
AbiyMelaku <agirma08@gmail.com>
2017-01-05 13:29:09 -0800

    all collection methods and listeners added. tests passed

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,35 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function (song) {
+      if (!this.at(1)) {  //this.at(0)
+        this.playFirst();
+      }
+    });
+
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      //plays the first song in the queue if there are any songs left
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+    
+    //when a song is dequeued remove the song
+    this.on('dequeue', function(song) {
+      this.remove(song);    
+    })
+  },
+
+  playFirst: function() {
+    //play method is from Song model
+    this.at(0).play();
+
+  },
+
+  
+  
+    
 
 });
